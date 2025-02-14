1cb0c953cbaa77db0c2b148cb78fe8a83b1f4f99
ArmanHossain04 <armanhossain0477@gmail.com>
2025-02-14 15:33:37 +0600

    modified looping.js and added task file

---
+++ looping.js
@@ -0,0 +1,10 @@
+const friends = ["elon", "bill", "mark", "waren"];
+for(const friend of friends){
+    console.log(friend);
+    
+}
+
+for(let i = 0; i<10; i++){
+    console.log(i);
+    
+}
---
+++ task-3.js
@@ -0,0 +1,8 @@
+var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+let concatenatedString = '';
+
+for (let element of numbers) {
+    concatenatedString += element;
+}
+
+console.log(concatenatedString);
